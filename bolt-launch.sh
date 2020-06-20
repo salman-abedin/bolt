670240895bdd6dda328a5550ed62deb3cac77189
Salman Abedin <salmanabedin@zohomail.com>
2020-06-20 08:24:05 +0600

    update

--- bolt-launch.sh
+++ bolt-launch.sh
@@ -3,7 +3,7 @@
 case $(file --mime-type "$1" -bL) in
 
     inode/directory)
-        $TERMINAL -e lf -last-dir-path ~/.config/lf/lastPath "$1"
+        $TERMINAL -e lf -last-dir-path ~/.config/lf/last_path "$1"
         ;;
 
     text/* | inode/x-empty | application/json | application/octet-stream)

135824ee98b1b216d7a142c50bc499c808808da7
salman-abedin <salman104alvi@gmail.com>
2020-08-16 10:11:21 +0600

    Added some optimizations

--- bolt.sh
+++ bolt.sh
@@ -10,107 +10,107 @@ SEARCHLIST=/tmp/searchlist
 # Modify this section according to your preference
 #========================================================
 launch() {
-    case $(file --mime-type "$1" -bL) in
-        #====================================================
-        # Find out the mimetype of the file you wannna launch
-        #====================================================
-        video/*)
-            #================================================
-            # Launch using your favorite programs
-            #================================================
-            mpv "$1"
-            ;;
-        #================================================
-        # So on and so forth
-        #================================================
-        application/pdf | application/epub+zip)
-            zathura "$1"
-            ;;
-        text/* | inode/x-empty | application/json | application/octet-stream)
-            "$TERMINAL" "$EDITOR" "$1"
-            # st "$EDITOR" "$*"
-            ;;
-        inode/directory)
-            "$TERMINAL" "$EXPLORER" "$*"
-            # st lf "$*"
-            ;;
-    esac
+   case $(file --mime-type "$1" -bL) in
+      #====================================================
+      # Find out the mimetype of the file you wannna launch
+      #====================================================
+      video/*)
+         #================================================
+         # Launch using your favorite programs
+         #================================================
+         mpv "$1"
+         ;;
+      #================================================
+      # So on and so forth
+      #================================================
+      application/pdf | application/epub+zip)
+         zathura "$1"
+         ;;
+      text/* | inode/x-empty | application/json | application/octet-stream)
+         "$TERMINAL" "$EDITOR" "$1"
+         # st "$EDITOR" "$*"
+         ;;
+      inode/directory)
+         "$TERMINAL" "$EXPLORER" "$*"
+         # st lf "$*"
+         ;;
+   esac
 }
 
 searchnlaunch() {
-    RESULT=$(grep "$1" "$SEARCHLIST" | head -1)
-    if [ -n "$RESULT" ]; then
-        launch "$RESULT"
-    else
-        "$BROWSER" google.com/search\?q="$1"
-    fi
+   RESULT=$(grep "$1" "$SEARCHLIST" | head -1)
+   if [ -n "$RESULT" ]; then
+      launch "$RESULT"
+   else
+      "$BROWSER" google.com/search\?q="$1"
+   fi
 }
 
 getconfig() {
-    while IFS= read -r line; do
-        case $line in
-            [[:alnum:]]* | /*) echo "$line" ;;
-        esac
-    done < "$1"
+   while IFS= read -r line; do
+      case $line in
+         [[:alnum:]]* | /*) echo "$line" ;;
+      esac
+   done < "$1"
 }
 
 rofisearch() {
-    QUERY=$(awk -F / '{print $(NF-1)"/"$NF}' "$SEARCHLIST" |
-        rofi -sort true -sorting-method fzf -dmenu -i -p Open) &&
-        searchnlaunch "$QUERY"
+   QUERY=$(awk -F / '{print $(NF-1)"/"$NF}' "$SEARCHLIST" |
+      rofi -sort true -sorting-method fzf -dmenu -i -p Open) &&
+      searchnlaunch "$QUERY"
 }
 
 tmuxsearch() {
-    if pidof tmux; then
-        tmux new-window
-    else
-        tmux new-session -d \; switch-client
-    fi
-    if pidof "$TERMINAL"; then
-        [ "$(pidof "$TERMINAL")" != "$(xdo pid)" ] &&
-            xdo activate -N Alacritty
-    else
-        "$TERMINAL" -e tmux attach &
-    fi
-    tmux send "$0 --fzf-search" "Enter"
+   if pidof tmux; then
+      tmux new-window
+   else
+      tmux new-session -d \; switch-client
+   fi
+   if pidof "$TERMINAL"; then
+      [ "$(pidof "$TERMINAL")" != "$(xdo pid)" ] &&
+         xdo activate -N Alacritty
+   else
+      "$TERMINAL" -e tmux attach &
+   fi
+   tmux send "$0 --fzf-search" "Enter"
 }
 
 fzfsearch() {
-    QUERY=$(awk -F / '{print $(NF-1)"/"$NF}' "$SEARCHLIST" |
-        fzf -e -i \
-            --reverse \
-            --border \
-            --margin 15%,25% \
-            --info hidden \
-            --bind=tab:down,btab:up \
-            --prompt "launch ") &&
-        searchnlaunch "$QUERY"
+   QUERY=$(awk -F / '{print $(NF-1)"/"$NF}' "$SEARCHLIST" |
+      fzf -e -i \
+         --reverse \
+         --border \
+         --margin 15%,25% \
+         --info hidden \
+         --bind=tab:down,btab:up \
+         --prompt "launch ") &&
+      searchnlaunch "$QUERY"
 }
 
 watch() {
-    grep -v "^#" ~/.config/bolt/paths |
-        xargs inotifywait -m -r -e create,delete,move |
-        while read -r line; do
-            generate
-        done &
+   grep -v "^#" ~/.config/bolt/paths |
+      xargs inotifywait -m -r -e create,delete,move |
+      while read -r line; do
+         generate
+      done &
 }
 
 generate() {
-    FILTERS=$(getconfig ~/.config/bolt/filters | awk '{printf "%s\\|",$0;}' | sed -e 's/|\./|\\./g' -e 's/\\|$//g')
-    getconfig ~/.config/bolt/paths |
-        xargs -I% find % -maxdepth $MAXDEPTH \
-            ! -regex ".*\($FILTERS\).*" > "$SEARCHLIST"
+   FILTERS=$(getconfig ~/.config/bolt/filters | awk '{printf "%s\\|",$0;}' | sed -e 's/|\./|\\./g' -e 's/\\|$//g')
+   getconfig ~/.config/bolt/paths |
+      xargs -I% find % -maxdepth $MAXDEPTH \
+         ! -regex ".*\($FILTERS\).*" > "$SEARCHLIST"
 }
 
 while :; do
-    case $1 in
-        --generate) generate ;;
-        --tmux-search) tmuxsearch ;;
-        --fzf-search) fzfsearch ;;
-        --launch) launch "$2" ;;
-        --rofi-search) rofisearch ;;
-        --watch) watch ;;
-        *) break ;;
-    esac
-    shift
+   case $1 in
+      --generate) generate ;;
+      --tmux-search) tmuxsearch ;;
+      --fzf-search) fzfsearch ;;
+      --launch) launch "$2" ;;
+      --rofi-search) rofisearch ;;
+      --watch) watch ;;
+      *) break ;;
+   esac
+   shift
 done
